a1ff1242ed2519069571d7a3969d18ea4d13dcbf
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-25 10:02:42 +0530

    Motion Animation

--- src/Project/Marquee.jsx
+++ src/Project/Marquee.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
-
-function Marquee({imgurls}) {
+import { motion } from 'framer-motion'
+function Marquee({imgurls,direction}) {
+  
   return (
-    <div className="flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden">
-        {imgurls.map(url => <img src={url} className='w-[6vw] flex-shrink-0'/>)} 
-        {imgurls.map(url => <img src={url} className='flex-shrink-0'/>)} 
-    </div>
+    <motion.div  className="flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden">
+        <motion.div initial={{x:direction==='left' ? "0" : "-100%"}} animate={{x:direction==="left" ? "-100%" : "0"}} transition={{ease:"linear",duration:15,repeat:Infinity}}  className="flex flex-shrink-0 gap-40 py-10 pr-40">
+        {imgurls.map(url => <img src={url} className=''/>)} 
+        </motion.div>
+        {/* <motion.div className="flex flex-shrink-0 gap-40 py-10 pr-10">
+        {imgurls.map(url => <img src={url} className=''/>)} 
+        </motion.div> */}
+        <motion.div initial={{x:direction==='left' ? "0" : "-100%"}} animate={{x:direction==="left" ? "-100%" : "0"}} transition={{ease:"linear",duration:15,repeat:Infinity}}  className="flex flex-shrink-0 gap-40 py-10 pr-40">
+        {imgurls.map(url => <img src={url} className=''/>)} 
+        </motion.div>
+    </motion.div>
   )
 }
 
--- src/Project/Marquees.jsx
+++ src/Project/Marquees.jsx
@@ -4,12 +4,13 @@ import Marquee from './Marquee'
 function Marquees() {
     var images = [
         ["https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275e12177716cb3f2ea_basf.svg","	https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg","	https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275909535ff431975cf_lavender.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg"],
+     
         ["https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275e12177716cb3f2ea_basf.svg","	https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275c2fcf540272672ef_intenseeye.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27518bb52e4d642644e_bcgp.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d2756e8df24e52915c71_flowcode.svg","	https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275e1d5aa08ccc379dd_ypo.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d275909535ff431975cf_lavender.svg","https://cdn.prod.website-files.com/6334198f239547d0f9cd84b3/65b2d27505e3a120466b87aa_singularity.svg"],
         
     ]
   return (
-    <div className= 'mt-20 py-20'>
-        {images.map(item => <Marquee imgurls={item}/>)}
+    <div className= 'mt-20 py-20 w-full relative overflow-hidden '>
+        {images.map((item,index) => <Marquee direction={index === 0 ? "left" : "right"} key = {index} imgurls={item}/>)}
        
     </div>
   )
