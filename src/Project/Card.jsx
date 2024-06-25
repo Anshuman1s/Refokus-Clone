a53b61630ce5a21918f13bba1a8f2ecee9432c76
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-25 10:08:47 +0530

    Card Animation

--- src/Project/Card.jsx
+++ src/Project/Card.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import { IoIosArrowRoundForward } from "react-icons/io";
-function Card({width,start,para,hover="none"}) {
+function Card({width,start,para,hover="flase"}) {
   return (
-    <div className={`bg-zinc-800 p-5 rounded-xl hover:${hover} ${width} min-h-[30rem] flex flex-col justify-between`}>
+    <motion.div whileHover={{backgroundColor:hover===true &&"#7443ff",padding:"25px"}} className={`bg-zinc-800 p-5 rounded-xl  ${width} min-h-[30rem] flex flex-col justify-between`}>
       <div className="w-full">
           
       <div className="w-full flex justify-between items-center">
@@ -28,7 +29,7 @@ function Card({width,start,para,hover="none"}) {
                     )
             }
         </div>
-    </div>
+    </motion.div>
   )
 }
 
