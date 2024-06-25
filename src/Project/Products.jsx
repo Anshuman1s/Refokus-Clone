8d353208950c1f4de1a4791482d83fd083f91515
Anshuman <anshumanshukla.12as@gmail.com>
2024-06-25 09:29:36 +0530

    Product Animation

--- src/Project/Products.jsx
+++ src/Project/Products.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Product from './Product'
+import { motion } from 'framer-motion'
 
 function Products() {
     var Products =[
@@ -9,10 +10,21 @@ function Products() {
         {name:"Yahoo",description:"Aruq Export Product its employees continue to receive orders from customer.",live:true,case:true},
         // {name:"",description:"Aruq Export Product its employees continue to receive orders from customer.",live:true,case:false},
     ]
+const [pos,setPostion] = useState(0)
+const mover = (val)=>{
+  setPostion(val*18)
+}
+    
   return (
-    <div className='mt-32'>
-        {Products.map((val,index)=> <Product val={val}/>)}
-      
+    <div className='mt-32 relative'>
+        {Products.map((val,index)=> <Product val={val} key={index} mover={mover} count={index}/>)}
+      <div className=" w-full h-full absolute top-0 pointer-events-none">
+        <motion.div initial={{y: pos, x:"-50%"}} animate={{y:pos + `rem`}} transition={{ease:[0.45, 0, 0.55, 1],duration:.5}} className="overflow-hidden absolute w-[32rem] h-[18rem] bg-white left-[45%] ">
+        <motion.div animate={{y: -pos + `rem`}} transition={{ease:[0.45, 0, 0.55, 1],duration:.5}} className=" w-full h-full bg-sky-500"></motion.div>
+        <motion.div animate={{y: -pos+ `rem`}} transition={{ease:[0.45, 0, 0.55, 1],duration:.5}} className=" w-full h-full bg-sky-500"></motion.div>
+        <motion.div animate={{y: -pos + `rem`}} transition={{ease:[0.45, 0, 0.55, 1],duration:.5}} className=" w-full h-full bg-sky-500"></motion.div>
+        </motion.div>
+      </div>
     </div>
   )
 }
